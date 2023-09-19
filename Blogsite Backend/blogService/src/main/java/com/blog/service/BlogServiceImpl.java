93f23b2d3b041a65c1a4d05256aafd56378e69a6
VyshnaviShriram <shriram.vyshnavi@gmail.com>
2023-09-19 14:57:36 +0530

    Revert "Initial commit"
    This reverts commit 6539322391b64e0a2a470cada678768d59d73600.

--- Blogsite Backend/blogService/src/main/java/com/blog/service/BlogServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.blog.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class BlogServiceImpl implements IBlogService{
-
-}
--- Blogsite Backend/userService/src/main/java/com/user/service/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.user.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class UserServiceImpl implements IUserService{
-
-}
